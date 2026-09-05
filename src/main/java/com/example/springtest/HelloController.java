218649ebe913daa3270d2bcf31aa2c52a98a638a
sonalishasahu <156523595+sonalishasahu@users.noreply.github.com>
2026-09-05 11:03:14 +0000

    Update hello message

--- src/main/java/com/example/springtest/HelloController.java
+++ src/main/java/com/example/springtest/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from Spring Boot";
+        return "Hello from Spring Boot - Version 2";
     }
 }
